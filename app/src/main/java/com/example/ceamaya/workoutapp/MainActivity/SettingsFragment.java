192a009b21afbd9df76f66b56b4e741d2e25ac00
cesar93683 <48188267+cesar93683@users.noreply.github.com>
2019-05-23 17:24:51 -0700

    separated activities into folders

---
+++ app/src/main/java/com/example/ceamaya/workoutapp/MainActivity/SettingsFragment.java
@@ -0,0 +1,29 @@
+package com.example.ceamaya.workoutapp.MainActivity;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ceamaya.workoutapp.R;
+
+public class SettingsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    public static Fragment newInstance() {
+        return new SettingsFragment();
+    }
+}
