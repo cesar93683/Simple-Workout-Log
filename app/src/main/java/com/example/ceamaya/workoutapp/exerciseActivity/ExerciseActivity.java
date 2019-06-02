52be6aee7945a962294770b991f5cdb4f7c1a7ca
cesar93683 <48188267+cesar93683@users.noreply.github.com>
2019-06-02 14:04:22 -0700

    removed placeholderFragment

--- app/src/main/java/com/example/ceamaya/workoutapp/exerciseActivity/ExerciseActivity.java
+++ app/src/main/java/com/example/ceamaya/workoutapp/exerciseActivity/ExerciseActivity.java
@@ -3,7 +3,6 @@ package com.example.ceamaya.workoutapp.exerciseActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -12,12 +11,8 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.example.ceamaya.workoutapp.ExerciseSet;
 import com.example.ceamaya.workoutapp.R;
@@ -31,6 +26,7 @@ public class ExerciseActivity extends AppCompatActivity implements SaveSets {
 
     private static final String EXTRA_EXERCISE_NAME = "EXTRA_EXERCISE_NAME";
     private static final String EXTRA_EXERCISE_ID = "EXTRA_EXERCISE_ID";
+    private final int INVALID_ID = -1;
     private ViewPager mViewPager;
     private int exerciseId;
     private String exerciseName;
@@ -48,12 +44,17 @@ public class ExerciseActivity extends AppCompatActivity implements SaveSets {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
         workoutLab = WorkoutLab.get(this);
-        exerciseId = getIntent().getIntExtra(EXTRA_EXERCISE_ID, 0);
+        exerciseId = getIntent().getIntExtra(EXTRA_EXERCISE_ID, INVALID_ID);
         exerciseName = getIntent().getStringExtra(EXTRA_EXERCISE_NAME);
+
+        if(exerciseId == INVALID_ID) {
+            finish();
+        }
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle(exerciseName);
+
         SectionsPagerAdapter mSectionsPagerAdapter =
                 new SectionsPagerAdapter(getSupportFragmentManager());
 
@@ -101,33 +102,6 @@ public class ExerciseActivity extends AppCompatActivity implements SaveSets {
             ((ExerciseFragment) fragment).onBackPressed();
         } else {
             super.onBackPressed();
-        }
-    }
-
-    public static class PlaceholderFragment extends Fragment {
-
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.activity_exercise_placeholder_fragment,
-                    container, false);
-            TextView textView = rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt
-                    (ARG_SECTION_NUMBER)));
-            return rootView;
         }
     }
 
