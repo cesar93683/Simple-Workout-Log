ed7889bf5c56e18b2e1212eac349a50412b787cf
cesar93683 <48188267+cesar93683@users.noreply.github.com>
2019-06-20 22:23:10 -0700

    disable vibration on timer

--- app/src/main/java/com/devcesar/workoutapp/exerciseActivity/ExerciseActivity.java
+++ app/src/main/java/com/devcesar/workoutapp/exerciseActivity/ExerciseActivity.java
@@ -217,7 +217,7 @@ public class ExerciseActivity extends AppCompatActivity implements SaveSets {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
       CharSequence name = getString(R.string.channel_name);
       String description = getString(R.string.channel_description);
-      int importance = NotificationManager.IMPORTANCE_DEFAULT;
+      int importance = NotificationManager.IMPORTANCE_LOW;
       NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
       channel.setDescription(description);
       NotificationManager notificationManager = getSystemService(NotificationManager.class);
