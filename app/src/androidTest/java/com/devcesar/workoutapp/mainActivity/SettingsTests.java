3692ae8f9c75161f758f26b8e8ad9aa04ca3161f
cesar93683 <48188267+cesar93683@users.noreply.github.com>
2019-06-21 16:14:45 -0700

    add auto start timer tests

--- app/src/androidTest/java/com/devcesar/workoutapp/mainActivity/SettingsTests.java
+++ app/src/androidTest/java/com/devcesar/workoutapp/mainActivity/SettingsTests.java
@@ -39,6 +39,8 @@ public class SettingsTests {
   public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(
       MainActivity.class);
 
+  // canDeleteAndImportAllExercisesCategoriesRoutines
+
   @Test
   public void canVisitSettingsTab() {
     ViewInteraction bottomNavigationItemView = onView(
@@ -61,25 +63,6 @@ public class SettingsTests {
                 0),
             isDisplayed()));
     textView.check(matches(withText("Settings")));
-  }
-
-  private static Matcher<View> childAtPosition(
-      final Matcher<View> parentMatcher, final int position) {
-
-    return new TypeSafeMatcher<View>() {
-      @Override
-      public void describeTo(Description description) {
-        description.appendText("Child at position " + position + " in parent ");
-        parentMatcher.describeTo(description);
-      }
-
-      @Override
-      public boolean matchesSafely(View view) {
-        ViewParent parent = view.getParent();
-        return parent instanceof ViewGroup && parentMatcher.matches(parent)
-            && view.equals(((ViewGroup) parent).getChildAt(position));
-      }
-    };
   }
 
   @Test
@@ -194,5 +177,24 @@ public class SettingsTests {
             0),
             isDisplayed()));
     linearLayout.check(doesNotExist());
+  }
+
+  private static Matcher<View> childAtPosition(
+      final Matcher<View> parentMatcher, final int position) {
+
+    return new TypeSafeMatcher<View>() {
+      @Override
+      public void describeTo(Description description) {
+        description.appendText("Child at position " + position + " in parent ");
+        parentMatcher.describeTo(description);
+      }
+
+      @Override
+      public boolean matchesSafely(View view) {
+        ViewParent parent = view.getParent();
+        return parent instanceof ViewGroup && parentMatcher.matches(parent)
+            && view.equals(((ViewGroup) parent).getChildAt(position));
+      }
+    };
   }
 }
