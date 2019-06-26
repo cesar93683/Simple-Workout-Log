aec8f9daaf3ca4ff684223eecb78a96f28fdaf6e
cesar93683 <48188267+cesar93683@users.noreply.github.com>
2019-06-25 21:40:10 -0700

    refactor MainActivityTests

--- app/src/androidTest/java/com/devcesar/workoutapp/mainActivity/MainActivityTests.java
+++ app/src/androidTest/java/com/devcesar/workoutapp/mainActivity/MainActivityTests.java
@@ -7,24 +7,20 @@ import static androidx.test.espresso.action.ViewActions.longClick;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static com.devcesar.workoutapp.mainActivity.ViewHelper.childAtPosition;
 import static com.devcesar.workoutapp.mainActivity.ViewHelper.sleepFor2Seconds;
 import static com.devcesar.workoutapp.mainActivity.ViewHelper.str_AlternatingDumbbellCurl;
+import static com.devcesar.workoutapp.mainActivity.ViewHelper.str_BarbellBackSquat;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.is;
 
 import android.content.pm.ActivityInfo;
-import android.view.ViewGroup;
-import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import com.devcesar.workoutapp.R;
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -50,9 +46,10 @@ public class MainActivityTests {
 
     onView(ViewMatchers.withText(ViewHelper.str_Save)).perform(click());
 
-    ViewInteraction textView2 = onView(withText(alternatingDumbbellCurl2));
-    textView2.check(matches(withText(alternatingDumbbellCurl2)));
-    textView2.perform(longClick());
+    onView(childAtPosition(withId(R.id.recycler_view), 0))
+        .check(matches(withText(alternatingDumbbellCurl2)));
+
+    onView(withText(alternatingDumbbellCurl2)).perform(longClick());
 
     onView(withId(R.id.edit_linear_layout)).perform(click());
 
@@ -60,9 +57,6 @@ public class MainActivityTests {
         .perform(replaceText(str_AlternatingDumbbellCurl), closeSoftKeyboard());
 
     onView(ViewMatchers.withText(ViewHelper.str_Save)).perform(click());
-
-    onView(withText(str_AlternatingDumbbellCurl)).check(matches(withText(
-        str_AlternatingDumbbellCurl)));
   }
 
   @Test
@@ -70,16 +64,8 @@ public class MainActivityTests {
     onView(allOf(withId(R.id.filter_edit_text), isDisplayed()))
         .perform(replaceText("barbell"), closeSoftKeyboard());
 
-    ViewInteraction textView = onView(
-        allOf(withText("Barbell Back Squat"),
-            childAtPosition(
-                allOf(withId(R.id.recycler_view),
-                    childAtPosition(
-                        withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
-                        1)),
-                0),
-            isDisplayed()));
-    textView.check(matches(withText("Barbell Back Squat")));
+    onView(childAtPosition(withId(R.id.recycler_view), 0))
+        .check(matches(withText(str_BarbellBackSquat)));
   }
 
   @Test
@@ -88,15 +74,7 @@ public class MainActivityTests {
 
     onView(ViewMatchers.withText(ViewHelper.str_Save)).perform(click());
 
-    ViewInteraction linearLayout = onView(
-        allOf(childAtPosition(
-            allOf(withId(R.id.text_input_layout),
-                childAtPosition(
-                    IsInstanceOf.instanceOf(android.widget.FrameLayout.class),
-                    0)),
-            1),
-            isDisplayed()));
-    linearLayout.check(matches(isDisplayed()));
+    onView(withId(R.id.textinput_error)).check(matches(withText("Please enter a name.")));
   }
 
   @Test
@@ -104,19 +82,12 @@ public class MainActivityTests {
     onView(allOf(withId(R.id.fab), isDisplayed())).perform(click());
 
     onView(withId(R.id.text_input_edit_text))
-        .perform(replaceText("Alternating Dumbbell Curl"), closeSoftKeyboard());
+        .perform(replaceText(str_AlternatingDumbbellCurl), closeSoftKeyboard());
 
     onView(ViewMatchers.withText(ViewHelper.str_Save)).perform(click());
 
-    ViewInteraction linearLayout = onView(
-        allOf(childAtPosition(
-            allOf(withId(R.id.text_input_layout),
-                childAtPosition(
-                    IsInstanceOf.instanceOf(android.widget.FrameLayout.class),
-                    0)),
-            1),
-            isDisplayed()));
-    linearLayout.check(matches(isDisplayed()));
+    onView(withId(R.id.textinput_error))
+        .check(matches(withText(str_AlternatingDumbbellCurl + " already exists.")));
   }
 
   @Test
@@ -127,15 +98,7 @@ public class MainActivityTests {
 
     onView(ViewMatchers.withText(ViewHelper.str_Save)).perform(click());
 
-    ViewInteraction linearLayout1 = onView(
-        allOf(childAtPosition(
-            allOf(withId(R.id.text_input_layout),
-                childAtPosition(
-                    IsInstanceOf.instanceOf(android.widget.FrameLayout.class),
-                    0)),
-            1),
-            isDisplayed()));
-    linearLayout1.check(matches(isDisplayed()));
+    onView(withId(R.id.textinput_error)).check(matches(withText("Same name.")));
   }
 
   @Test
@@ -150,15 +113,7 @@ public class MainActivityTests {
 
     onView(allOf(withId(R.id.filter_edit_text), isDisplayed())).check(matches(withText("sq")));
 
-    ViewInteraction textView = onView(
-        allOf(withText("Barbell Back Squat"),
-            childAtPosition(
-                allOf(withId(R.id.recycler_view),
-                    childAtPosition(
-                        IsInstanceOf.instanceOf(ViewGroup.class),
-                        1)),
-                0),
-            isDisplayed()));
-    textView.check(matches(withText("Barbell Back Squat")));
+    onView(childAtPosition(withId(R.id.recycler_view), 0))
+        .check(matches(withText(str_BarbellBackSquat)));
   }
 }
